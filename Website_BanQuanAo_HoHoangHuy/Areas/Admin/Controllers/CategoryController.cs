1183545051e71c910b2e044769492ce7f848614f
hohoanghuy13 <hohoanghuy170@gmail.com>
2023-10-10 16:27:01 +0700

    Xong giao dien lan 2

---
+++ Website_BanQuanAo_HoHoangHuy/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_BanQuanAo_HoHoangHuy.Models;
+namespace Website_BanQuanAo_HoHoangHuy.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ShopDBContext db = new ShopDBContext();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            List<LoaiSanPham> loaiSP = db.LoaiSanPhams.ToList();
+            ViewBag.LoaiSP = loaiSP;
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LoaiSanPham loaiSP)
+        {
+            if (loaiSP == null)
+                return View();
+            else
+            {
+                db.LoaiSanPhams.Add(loaiSP);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category", "Admin");
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            LoaiSanPham loaiSP = db.LoaiSanPhams.Where(row => row.MaLoaiSP == id).FirstOrDefault();
+            return View(loaiSP);
+        }
+        [HttpPost]
+        public ActionResult Edit(LoaiSanPham loaiSP)
+        {
+            if (loaiSP == null)
+                return RedirectToAction("Index", "Category", "Admin");
+            else
+            {
+                LoaiSanPham newLoaiSP = db.LoaiSanPhams.Where(row => row.MaLoaiSP == loaiSP.MaLoaiSP).FirstOrDefault();
+                newLoaiSP.TenLoaiSP = loaiSP.TenLoaiSP;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category", "Admin");
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            LoaiSanPham loaiSP = db.LoaiSanPhams.Where(row => row.MaLoaiSP == id).FirstOrDefault();
+            db.LoaiSanPhams.Remove(loaiSP);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category", "Admin");
+        }
+
+    }
+}
--- Website_BanQuanAo_HoHoangHuy/Controllers/ProductController.cs
+++ Website_BanQuanAo_HoHoangHuy/Controllers/ProductController.cs
@@ -11,14 +11,58 @@ namespace Website_BanQuanAo_HoHoangHuy.Controllers
     {
         ShopDBContext db = new ShopDBContext();
         // GET: Product
-        public ActionResult Index()//Hien thi tat ca san pham - truyen vao string search
+        public ActionResult Index(string search = "", string Sort = "TenSanPham", string Icon = "", int page = 1)
         {
-            List<SanPham> sanPham = db.SanPhams.ToList();//Them chuc nang tim kiem
+            List<SanPham> sanPham = db.SanPhams.Where(row => row.TenSanPham.Contains(search)).ToList();
+            ViewBag.Search = search;
+            ViewBag.Sort = Sort;
+            ViewBag.Icon = Icon;
+            if (Sort == "TenSanPham")
+            {
+                if (Icon == "fa fa-sort-down")
+                    sanPham = sanPham.OrderByDescending(row => row.TenSanPham).ToList();
+                else
+                    sanPham = sanPham.OrderBy(row => row.TenSanPham).ToList();
+            }
+            if(Sort == "Gia")
+            {
+                if (Icon == "fa fa-sort-down")
+                    sanPham = sanPham.OrderByDescending(row => row.Gia).ToList();
+                else
+                    sanPham = sanPham.OrderBy(row => row.Gia).ToList();
+            }
+            int NoOfRecordPerPage = 4;
+            double countFoods = sanPham.Count;
+            int NoOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(countFoods) / Convert.ToDouble(NoOfRecordPerPage)));
+            int NoOfSkip = (page - 1) * NoOfRecordPerPage;
+            ViewBag.Page = page;
+            ViewBag.NoOfPages = NoOfPages;
+            sanPham = sanPham.Skip(NoOfSkip).Take(NoOfRecordPerPage).ToList();
             return View(sanPham);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            else
+            {
+                SanPham sp = db.SanPhams.Where(row => row.MaSanPham == id).FirstOrDefault();
+                return View(sp);
+            }    
         }
         public ActionResult T_Shirt()
         {
             List<SanPham> sanPham = db.SanPhams.Where(row => row.MaLoaiSP == 1).ToList();
+            return View(sanPham);
+        }
+        public ActionResult Shirt()
+        {
+            List<SanPham> sanPham = db.SanPhams.Where(row => row.MaLoaiSP == 2).ToList();
+            return View(sanPham);
+        }
+        public ActionResult Polo()
+        {
+            List<SanPham> sanPham = db.SanPhams.Where(row => row.MaLoaiSP == 3).ToList();
             return View(sanPham);
         }
     }
