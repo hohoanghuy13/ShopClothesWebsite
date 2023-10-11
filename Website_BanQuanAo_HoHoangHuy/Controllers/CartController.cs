30b2eceb86d8695f61dc0e6fa395d9ed93e45104
hohoanghuy13 <hohoanghuy170@gmail.com>
2023-10-11 15:50:49 +0700

    Dang them gio hang, Kho

---
+++ Website_BanQuanAo_HoHoangHuy/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_BanQuanAo_HoHoangHuy.Models;
+
+namespace Website_BanQuanAo_HoHoangHuy.Controllers
+{
+    public class CartController : Controller
+    {
+        ShopDBContext db = new ShopDBContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            List<ChiTietHoaDon> cthd = db.ChiTietHoaDons.Where(row => row.MaHoaDon == 1).ToList();
+            return View(cthd);
+        }
+    }
+}
--- Website_BanQuanAo_HoHoangHuy/Controllers/ProductController.cs
+++ Website_BanQuanAo_HoHoangHuy/Controllers/ProductController.cs
@@ -50,6 +50,41 @@ namespace Website_BanQuanAo_HoHoangHuy.Controllers
                 return View(sp);
             }    
         }
+        [HttpPost]
+        public ActionResult AddToCart(ChiTietHoaDon sp, HoaDon hd)
+        {
+            if (hd.KhachHang != null)
+            {
+                HoaDon hoaDonCuaKH = db.HoaDons.Where(row => row.KhachHang == hd.KhachHang && row.TrangThaiDonHang == "Chưa thanh toán").FirstOrDefault();
+                ChiTietHoaDon themVaoGio = new ChiTietHoaDon();
+
+                List<HoaDon> lastBill = db.HoaDons.ToList();
+                if (lastBill.Count == 0)
+                    hoaDonCuaKH.MaHoaDon = 1;
+                else
+                    hoaDonCuaKH.MaHoaDon = lastBill.Count + 1;
+                if (hoaDonCuaKH == null)
+                {
+                    hoaDonCuaKH.KhachHang = hd.KhachHang;
+                    hoaDonCuaKH.NgayMua = DateTime.Now.Date;
+                    hoaDonCuaKH.TongTien = 0;
+                    hoaDonCuaKH.TrangThaiDonHang = "Chưa thanh toán";
+                    db.HoaDons.Add(hoaDonCuaKH);
+                    db.SaveChanges();
+                }
+                hoaDonCuaKH = db.HoaDons.Where(row => row.KhachHang == hd.KhachHang && row.TrangThaiDonHang == "Chưa thanh toán").FirstOrDefault();
+                themVaoGio.MaHoaDon = hoaDonCuaKH.MaHoaDon;
+                themVaoGio.MaSanPham = sp.MaSanPham;
+                themVaoGio.SoLuong = sp.SoLuong;
+                themVaoGio.Gia = sp.Gia;
+                themVaoGio.ThanhTien = sp.SoLuong * sp.Gia;
+                hoaDonCuaKH.TongTien += themVaoGio.ThanhTien;
+                db.ChiTietHoaDons.Add(themVaoGio);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Cart");
+            }
+            return RedirectToAction("Index");
+        }
         public ActionResult T_Shirt()
         {
             List<SanPham> sanPham = db.SanPhams.Where(row => row.MaLoaiSP == 1).ToList();
